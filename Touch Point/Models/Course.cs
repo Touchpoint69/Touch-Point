0f9669c589be94a6120f158e160215f4207873ca
DanielKarstens <dkhaha19@hotmail.com>
2017-11-23 11:26:44 +0100

    Noget binding ting der ikke paavirker noget andet ( forhåbenlig)

--- Touch Point/Models/Course.cs
+++ Touch Point/Models/Course.cs
@@ -1,23 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Touch_Point
 {
-    class Course
+    class Course : INotifyPropertyChanged
     {
         int _courseID;
         string _name;
         DateTimeOffset _time;
         string _teacher;
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public Course(int _courseID,string _name, DateTimeOffset _time, string _teacher)
         {
             
         }
+        public List <string> Courses
+        {
+            get { return Courses; }
+            
+        }
         
-        
+
+
     }
 }

a686ed69495bf327632282b9ced05220c111668b
DanielKarstens <dkhaha19@hotmail.com>
2017-11-28 14:51:32 +0100

    View model class tilføjet og binding til Course ID

--- Touch Point/Models/Course.cs
+++ Touch Point/Models/Course.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Touch_Point
 {
-    class Course : INotifyPropertyChanged
+    public class Course
     {
         int _courseID;
         string _name;
@@ -24,44 +24,22 @@ namespace Touch_Point
             //Sampletid skal erstattes af datetime
             _teacher = teacher; // teacher skal være objektet teacher ikke en string
         }
-        private ObservableCollection<Course> _courses;
 
-        public ObservableCollection <Course> Courses
-        {
-            get { return _courses; }
-            set { _courses = value; }
-            
-        }
         //ObservableCollection<Course> CourseList = new ObservableCollection<Course>();
         //Course C1 = new Course(11,"Zoneterapi",12/12/12,"Jørgen"); 
-        
-        public void AddCourses()
-        {
-            //CourseList.Add(C1);
-        }
 
         public Course()
         {
-            Courses = new ObservableCollection<Course>();
-            Course C1 = new Course(11, "Zoneterapi", 12 / 12 / 12, "Jørgen");
-            Course C2 = new Course(11, "Ninja", 12 / 12 / 12, "Bolette");
-            Courses.Add(C1);
-            Courses.Add(C2);
         }
+
+        public int CourseId
+        {
+            get { return _courseID; }
+        }
+
         public override string ToString()
         {
             return "This is a " + _name + " Kursus Afholdt af " + _teacher;
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged
-            ([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this,
-                new PropertyChangedEventArgs(propertyName));
-        }
-
-
     }
 }
---
+++ Touch Point/ViewModels/Domain/Course/CourseMDVM.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Touch_Point.ViewModels.Domain.Course
+{
+    public class CourseMDVM : INotifyPropertyChanged
+    {
+        public CourseMDVM()
+        {
+            Courses = new ObservableCollection<Touch_Point.Course>();
+            Touch_Point.Course C1 = new Touch_Point.Course(11, "Zoneterapi", 12 / 12 / 12, "Jørgen");
+            Touch_Point.Course C2 = new Touch_Point.Course(14, "Ninja", 12 / 12 / 12, "Bolette");
+            Courses.Add(C1);
+            Courses.Add(C2);
+        }
+        private ObservableCollection<Touch_Point.Course> _courses;
+        private Touch_Point.Course _selectedCourse;
+
+        public ObservableCollection<Touch_Point.Course> Courses
+        {
+            get { return _courses; }
+            set { _courses = value; }
+
+        }
+
+        public Touch_Point.Course SelectedCourse
+        {
+            get { return _selectedCourse; }
+            set
+            {
+                _selectedCourse = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

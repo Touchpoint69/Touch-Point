f4aa9765074aaa0f984961da1b3dfd37bbdec234
HassanRH <hassan---06@hotmail.com>
2017-12-12 12:56:31 +0100

    Delete i Teacher

--- Touch Point/Models/TeacherCatalog.cs
+++ Touch Point/Models/TeacherCatalog.cs
@@ -7,8 +7,34 @@ using System.Threading.Tasks;
 
 namespace Touch_Point.Models
 {
-    class TeacherCatalog
+  
+
+  public class TeacherCatalog
     {
-       
+        private System.Collections.ObjectModel.ObservableCollection<Touch_Point.Teacher> _data;
+
+        public TeacherCatalog()
+        {
+            _data = new ObservableCollection<Teacher>();
+        }
+
+        public ObservableCollection<Teacher> Data { get => _data; set => _data = value; }
+
+        public void Create(Touch_Point.Teacher newStudent)
+        {
+            _data.Add(newStudent);
+        }
+
+        public void Delete(int id)
+        {
+            for (int i = 0; i < _data.Count; i++)
+            {
+                if (_data[i].TeacherID == id)
+                {
+                    _data.RemoveAt(i);
+                    return;
+                }
+            }
+        }
     }
 }

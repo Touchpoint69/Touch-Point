906166f425f4d0a4ea51d624d088a682c3e63376
ProfessorGenki <getyowalkon@gmail.com>
2017-12-14 12:41:44 +0100

    Timeld og afmeld

--- Touch Point/Views/TilmeldKursist.xaml.cs
+++ Touch Point/Views/TilmeldKursist.xaml.cs
@@ -13,33 +13,23 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Touch_Point
+namespace Touch_Point.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class TilmeldKursist : Page
     {
-        public MainPage()
+        public TilmeldKursist()
         {
             this.InitializeComponent();
         }
 
         private void _AdminButton__Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(Views.Admin));
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(Views.TilmeldKursist));
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(Views.AfmeldKursist));
+            Frame.Navigate(typeof(MainPage));
         }
     }
 }
